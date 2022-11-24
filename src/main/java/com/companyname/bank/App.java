36d68fb2d02f4c1c522459d7925ed5153f70b952
vamshikarnekota <107358914+vamshikarnekota@users.noreply.github.com>
2022-11-24 22:45:40 +0530

    2build web hook trigger

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "build web hook trigger!" );
+        System.out.println( "git web hook trigger!" );
     }
 }
